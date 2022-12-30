b6af1f8e620f218f132fe1e2209ecb39a00cd650
Charles <bell.charles1402@gmail.com>
2022-12-30 16:56:03 -0500

    adding timeout function

--- auto.py
+++ auto.py
@@ -3,7 +3,14 @@
 import sys
 import os
 import glob
+from threading import Timer
 
+def exit():
+    print("Exiting due to timeout...")
+    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm -r eeToZgamma/Events/*'")
+    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm eeToZgamma/RunWeb'")
+    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm Delphes-3.5.0/Frag_Study/*'")
+    os._exit(os.EX_OK)
 
 try:
     input = raw_input
@@ -13,6 +20,11 @@ except:
 if len(sys.argv) < 2:
     print(" Usage: auto.py results_name")
     sys.exit(1)
+
+# Starts a timer to end the script after ten minutes
+timer = Timer(300, exit)
+timer.start()
+
 
 results_name = sys.argv[1]
 
@@ -29,6 +41,8 @@ elif os.path.exists(glob.glob("eeToZgamma/Events/run_01/*hepmc.gz")[0]):
     os.system("gunzip Delphes-3.5.0/Frag_Study/pythia8_events_"+results_name+".hepmc.gz")
 else:
     sys.exit("No hepmc file")
+
+timer.cancel()
 # Runs .hepmc through Delphes creating a .root
 os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes "
           "'./Delphes-3.5.0/DelphesHepMC2 Delphes-3.5.0/cards/delphes_card_CMS.tcl "
@@ -47,3 +61,6 @@ os.system("docker run --rm -i -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg
 # Removes .hepmc and .root files
 os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm "
           "Delphes-3.5.0/Frag_Study/*'")
+# Displays profx.png
+# os.system("pwd")
+# os.system("display ../FragmentationStudy/plots/eeToZgamma_"+results_name+"_PartVsPprofx.png")
