df3ab4e65916580c2c01615bd06b58c39ba9b578
Charles <bell.charles1402@gmail.com>
2023-01-04 14:12:52 -0500

    fixing -i issue

--- auto.py
+++ auto.py
@@ -7,9 +7,11 @@ from threading import Timer
 
 def exit():
     print("Exiting due to timeout...")
-    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm -r eeToZgamma/Events/*'")
-    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm eeToZgamma/RunWeb'")
-    os.system("docker run --rm -ti -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm Delphes-3.5.0/Frag_Study/*'")
+    os.system("docker run --rm -t -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm -r eeToZgamma/Events/*'")
+    os.system("docker run --rm -t -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm eeToZgamma/RunWeb'")
+    os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm Delphes-3.5.0/Frag_Study/*'")
+    os.system("docker run --rm -t -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'exit'")
+    os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'exit'")
     os._exit(os.EX_OK)
 
 try:
@@ -22,7 +24,7 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 # Starts a timer to end the script after ten minutes
-timer = Timer(300, exit)
+timer = Timer(150, exit)
 timer.start()
 
 
@@ -31,7 +33,7 @@ results_name = sys.argv[1]
 print("Beginning...")
 
 # Generates events after parameter change
-os.system("docker run --rm -i -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 "
+os.system("docker run --rm -t -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 "
           "'./eeToZgamma/bin/generate_events -f'")
 # Copies and unzips new .hepmc
 if len(glob.glob("eeToZgamma/Events/run_01/*hepmc.gz")) == 0:
@@ -44,22 +46,22 @@ else:
 
 timer.cancel()
 # Runs .hepmc through Delphes creating a .root
-os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes "
+os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes "
           "'./Delphes-3.5.0/DelphesHepMC2 Delphes-3.5.0/cards/delphes_card_CMS.tcl "
           "Delphes-3.5.0/Frag_Study/eeToZgamma_"+results_name+".root "
           "Delphes-3.5.0/Frag_Study/pythia8_events_"+results_name+".hepmc'")
 # Creates plots from the new .root
-os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'cd Delphes-3.5.0 && "
+os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'cd Delphes-3.5.0 && "
           "python ../FragmentationStudy/loop.py Frag_Study/eeToZgamma_"+results_name+".root "
           "eetoZgamma_"+results_name+"'")
 # Removes old run files
-os.system("docker run --rm -i -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm -r "
+os.system("docker run --rm -t -v $PWD:$PWD -w $PWD scailfin/madgraph5-amc-nlo:mg5_amc3.3.1 'rm -r "
           "eeToZgamma/Events/run_*'")
 # Removes .hepmc files but preserves .root files
-# os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm "
+# os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm "
 #           "Delphes-3.5.0/Frag_Study/pythia8_events_"+results_name+".hepmc'")
 # Removes .hepmc and .root files
-os.system("docker run --rm -i -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm "
+os.system("docker run --rm -t -v $PWD:$PWD -w $PWD ghcr.io/scipp-atlas/mario-mapyde/delphes 'rm "
           "Delphes-3.5.0/Frag_Study/*'")
 # Displays profx.png
 # os.system("pwd")
