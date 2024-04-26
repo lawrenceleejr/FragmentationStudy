af0f2a08064934985dd957bccda3f20b764d1b6e
jslawless <johnnylawless44@gmail.com>
2024-04-26 09:26:16 -0400

    2d hist

--- LEPskeleton/jet_distributions.py
+++ LEPskeleton/jet_distributions.py
@@ -15,9 +15,13 @@ def jet_distributions(inFile,outTag):
     f = uproot4.open(inFile)
     tree = f["akR4ESchemeJetTree"]
     branches = tree.arrays()
+    energy = f["t"].arrays()["Energy"]
 
+    
     dijet_m = ROOT.TH1D("dijet_m","Leading Dijet Invariant Mass",120,0,120)
+    d_v_E = ROOT.TH2D("d_v_E","Leading Dijet Invariant Mass vs Energy",100,0,120,100,0,120)
 
+    print(len(energy))
     nref = branches["nref"]
     jtpt = branches["jtpt"]
     jteta = branches["jteta"]
@@ -37,9 +41,13 @@ def jet_distributions(inFile,outTag):
         jet2 = ROOT.TLorentzVector()
         jet2.SetPtEtaPhiM(jtpt[i][1],jteta[i][1],jtphi[i][1],jtm[i][1])
         
-        dijet_m.Fill((jet1 + jet2).M())
+
+        inv_m = (jet1 + jet2).M()
+        dijet_m.Fill(inv_m)
+        d_v_E.Fill(inv_m,energy[i])
 
     savehist(dijet_m,"dijet_m",myfile)
+    savehist(d_v_E,"d_v_E",myfile)
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
