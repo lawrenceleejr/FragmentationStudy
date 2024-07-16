fd8acd724e6e0f1ccfa4abe35f47d0f95be1ebad
Lawrence Lee <lawrenceleejr@gmail.com>
2024-07-16 15:33:40 -0400

    flipping to have it be a quark tagger

--- plots/paper_roc.py
+++ plots/paper_roc.py
@@ -39,16 +39,16 @@ def plot_roc_curve(fpr1, tpr1, fpr2, tpr2, title='ROC Curve', output_filename='r
     N = len(fpr1)-1
     # Discretize the colormap into N segments
 
-    goodDataStart = np.argmax(fpr1 < 0.999)
-    goodDataEnd = np.argmax(fpr1 < 0.0001)
+    goodDataStart = np.argmax(fpr1 > 0.0001)
+    goodDataEnd = np.argmax(fpr1 > 0.999)
 
     N = goodDataEnd - goodDataStart
 
     print(goodDataStart,goodDataEnd)
 
     colors = [(1,1,1,0) for i in range(goodDataStart)]
-    colors += [plasma_cmap( i/N ) for i in range(N)]
-    colors += [(1,1,1,0) for i in range(goodDataEnd,len(fpr1))]
+    colors += [plasma_cmap( i/N ) for i in range(0,N-8)[::-1]]
+    colors += [(1,1,1,0) for i in range(len(fpr1))]
     print(colors)
 
 
@@ -61,32 +61,36 @@ def plot_roc_curve(fpr1, tpr1, fpr2, tpr2, title='ROC Curve', output_filename='r
 
 
     plt.plot(fpr1, tpr1, "-", color='black', lw=2, label=r'$ee\rightarrow ZZ\rightarrow 4j$')
-    plt.plot(fpr2, tpr2, ":", color='black', lw=2, label=r'$ee\rightarrow 3j$')
+    plt.plot(fpr2, tpr2, "--", color='black', lw=2, label=r'$ee\rightarrow 3j$')
 
-    highlightPoint = 10
+    highlightPoint = 8
 
-    plt.plot([fpr1[highlightPoint],fpr2[highlightPoint]], [tpr1[highlightPoint],tpr2[highlightPoint]], "-o", color='black', lw=2,mew=2,mec="k",mfc="w",ms=7)
+    plt.annotate(r'$\epsilon_q=$'+f"{tpr1[highlightPoint]:0.2f}  ", xy=(fpr1[highlightPoint],tpr1[highlightPoint]), 
+        xytext=(fpr1[highlightPoint]+0.15,tpr1[highlightPoint]),
+                 arrowprops=dict(facecolor='black', arrowstyle='-',ls=":"),
+                 fontsize=14, color='k',horizontalalignment='left',verticalalignment='center')
+
+    plt.annotate(r'$\epsilon_q=$'+f"{tpr2[highlightPoint]:0.2f}  ", xy=(fpr2[highlightPoint],tpr2[highlightPoint]), 
+        xytext=(fpr2[highlightPoint]+0.15,tpr2[highlightPoint]),
+                 arrowprops=dict(facecolor='black', arrowstyle='-',ls=":"),
+                 fontsize=14, color='k',horizontalalignment='left',verticalalignment='center')
+
+
+    plt.plot([fpr1[highlightPoint],fpr2[highlightPoint]], [tpr1[highlightPoint],tpr2[highlightPoint]], "-o", color='k', lw=2,mew=2,mec="k",mfc="w",ms=7,zorder=100)
     # plt.plot([0, 1], [0, 1], color='black', lw=1, linestyle='-')
-
-    plt.annotate(r'$\epsilon_q=$'+f"{fpr1[highlightPoint]:0.2f}  ", xy=(fpr1[highlightPoint],tpr1[highlightPoint]), 
-        xytext=(fpr1[highlightPoint],tpr1[highlightPoint]),
-                 # arrowprops=dict(facecolor='black', arrowstyle='->'),
-                 fontsize=14, color='k',horizontalalignment='right')
-
-    plt.annotate(r'  $\epsilon_q=$'+f"{fpr2[highlightPoint]:0.2f}  ", xy=(fpr2[highlightPoint],tpr2[highlightPoint]), 
-        xytext=(fpr2[highlightPoint],tpr2[highlightPoint]),
-                 # arrowprops=dict(facecolor='black', arrowstyle='->'),
-                 fontsize=14, color='k',horizontalalignment='left',verticalalignment='top')
-
 
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
-    plt.xlabel('Quark Efficiency')
-    plt.ylabel('Gluon Efficiency')
-    # plt.title(title)
-    plt.legend(loc="lower right")
 
-    plt.text(0.5, 0.3, f'Pythia 8.306\n'+r"$n_{90}$-based $q/g$ Tagger"+f'\n{ptrange} GeV Jets\n∆AUC: {-AUC1+AUC2:0.2f}\nPointwise Distance: {euclideandistance:0.2f}', fontsize=14, color='black')
+    # plt.xscale('log')  # Set x-axis to log scale
+    # plt.xlim(0.001, 1)  # Adjust x-axis limits accordingly
+
+    plt.xlabel('Gluon Efficiency')
+    plt.ylabel('Quark Efficiency')
+    # plt.title(title)
+    plt.legend(loc="lower right", fancybox=False, frameon=False, framealpha=0, shadow=True, borderpad=1,fontsize=14)
+
+    plt.text(0.5, 0.4, f'Pythia 8.306\n'+r"$n_{90}$-based $q/g$ Tagger"+f'\n{ptrange} GeV Jets\n∆AUC: {AUC1-AUC2:0.2f}\nPointwise Distance: {euclideandistance:0.2f}', fontsize=14, color='black')
     # Save the plot as a PDF
 
     plt.tight_layout()  
@@ -105,8 +109,15 @@ if __name__ == "__main__":
         fpr2 = JJJdata[0]
         tpr2 = JJJdata[1]
 
+        # this is currently set up as a gluon tagger... (i.e. selecting jets with n90>cut). 
+
+        fpr1 = 1-fpr1
+        tpr1 = 1-tpr1
+        fpr2 = 1-fpr2
+        tpr2 = 1-tpr2
+
         print(fpr1,tpr1,fpr2,tpr2)
 
-        plot_roc_curve(fpr1, tpr1, fpr2, tpr2, output_filename=f'roc_curve_{ptrange}.pdf')
+        plot_roc_curve(tpr1, fpr1, tpr2, fpr2, output_filename=f'roc_curve_{ptrange}.pdf')
 
 
